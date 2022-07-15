2e6024fb40b64f56cf900712c4334a40637a61e8
mohammad azad <mohammadazad.bc@gmail.com>
2022-07-15 18:25:07 +0200

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import "./contact.scss"
 
 function Contact() {
+
+  const [message, setMessage] = useState(false)
+
+  const handleSumbit =(e)=>{
+    e.preventDefault();
+    setMessage(true)
+  }
   return (
-    <div className="contact" id="contact"> Contact</div>
+    <div className="contact" id="contact"> 
+    <div className="left">
+      <img src="assete/shake.svg" alt="" />
+    </div>
+    <div className="right">
+      <h2>Contact.</h2>
+        <form onSubmit={handleSumbit}>
+          <input type="text" placeholder="Email" />
+          <textarea type="text" placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :) </span>}
+        </form>
+    </div>
+    </div>
   )
 }
 
